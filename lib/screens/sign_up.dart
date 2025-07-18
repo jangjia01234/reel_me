aa673015b08493101301b61228bb001c772c2faa
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-07-18 22:47:54 +0900

    removed unnecessary comments

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -64,15 +64,6 @@ class SignUpScreen extends StatelessWidget {
             ],
           ),
         ),
-        // color: Colors.grey.shade50,
-        // elevation: 2,
-        // child: Padding(
-        //   padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
-        //   child: Text(
-        //     "Already have an account?",
-        //     style: TextStyle(fontSize: Sizes.size16, color: Colors.black),
-        //   ),
-        // ),
       ),
     );
   }
