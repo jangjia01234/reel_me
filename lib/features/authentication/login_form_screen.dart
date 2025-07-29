adf2876b72f577f55ef99d906364f719d1d14d30
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-07-29 22:18:00 +0900

    Login Form 완성

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -12,6 +12,21 @@ class LoginFormScreen extends StatefulWidget {
 }
 
 class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Map<String, String> formData = {};
+
+  void _onSubmitTap() {
+    // currentState가 있어야 validate 가능
+    if (_formKey.currentState != null) {
+      if (_formKey.currentState!.validate()) {
+        // 에러가 없을 경우
+        // Form을 save하면 모든 텍스트 입력에 대해 onSaved 함수 실행
+        _formKey.currentState!.save();
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,14 +36,50 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              TextFormField(),
+              Gaps.v28,
+              TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                  ),
+                  validator: (value) {
+                    // error message
+                    return null;
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['email'] = newValue;
+                    }
+                  }),
               Gaps.v16,
-              TextFormField(),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                validator: (value) {
+                  // error message
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
               Gaps.v28,
               // TODO: 'Next' 대신 다른 텍스트도 넣을 수 있도록 위젯 확장
-              FormButton(disabled: false),
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: FormButton(disabled: false),
+              ),
             ],
           ),
         ),
