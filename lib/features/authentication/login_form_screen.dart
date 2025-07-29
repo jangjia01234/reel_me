563fb6a96ed0bd0c155adcdec8fb04e74231bf9f
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-07-29 21:28:03 +0900

    TODO 추가 (FormButton 확장)

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:reel_me/constants/gaps.dart';
+import 'package:reel_me/features/authentication/widgets/form_button.dart';
+
+import '../../constants/sizes.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -11,7 +15,24 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("login form"),
+      appBar: AppBar(
+        title: Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(),
+              Gaps.v16,
+              TextFormField(),
+              Gaps.v28,
+              // TODO: 'Next' 대신 다른 텍스트도 넣을 수 있도록 위젯 확장
+              FormButton(disabled: false),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
