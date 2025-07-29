72541ff0390a2d0dd4b298d1b0cb359430230822
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-07-29 16:55:25 +0900

    birthday screen UI 구현

--- lib/authentication/birthday_screen.dart
+++ lib/authentication/birthday_screen.dart
@@ -1,12 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:reel_me/authentication/onbording_screen.dart';
+import 'package:reel_me/authentication/widgets/form_button.dart';
 
-class BirthdayScreen extends StatelessWidget {
+import '../constants/gaps.dart';
+import '../constants/sizes.dart';
+
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
+
+  @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = ""; // private 처리를 위해 _ 표시
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    if (_username.isEmpty) return;
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => OnbordingScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("birthday screen"),
+      appBar: AppBar(
+        title: Text(
+          "Sign up",
+          style: TextStyle(fontSize: Sizes.size16, fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v20,
+              Text(
+                "When's your birthday?",
+                style: TextStyle(
+                  fontSize: Sizes.size16 + Sizes.size2,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              Gaps.v4,
+              Text(
+                "Your birthday won't be shown publicly.",
+                style: TextStyle(
+                  fontSize: Sizes.size14,
+                  fontWeight: FontWeight.w300,
+                  color: Colors.black54,
+                ),
+              ),
+              Gaps.v10,
+              TextFormField(
+                controller: _usernameController,
+                cursorColor: Theme.of(context).primaryColor,
+                decoration: InputDecoration(
+                  hintText: "Username",
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey.shade400),
+                  ),
+                ),
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onNextTap,
+                child: FormButton(disabled: _username.isEmpty),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/authentication/onbording_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnbordingScreen extends StatelessWidget {
+  const OnbordingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("onbording screen"),
+    );
+  }
+}
