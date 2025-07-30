56c7bbd2b9d31e04d6a30a47e9d81a1924d97b59
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-07-30 15:55:05 +0900

    Animated button 구현

--- lib/features/onboarding/widgets/interest_button.dart
+++ lib/features/onboarding/widgets/interest_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/sizes.dart';
 
-class InterestButton extends StatelessWidget {
+class InterestButton extends StatefulWidget {
   const InterestButton({
     super.key,
     required this.interest,
@@ -11,27 +11,45 @@ class InterestButton extends StatelessWidget {
   final String interest;
 
   @override
+  State<InterestButton> createState() => _InterestButtonState();
+}
+
+class _InterestButtonState extends State<InterestButton> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    setState(() {
+      _isSelected = !_isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(
-          vertical: Sizes.size16, horizontal: Sizes.size20),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(Sizes.size32),
-          color: Colors.white,
-          border: Border.all(
-            color: Colors.black.withValues(alpha: 0.1),
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(
+            vertical: Sizes.size16, horizontal: Sizes.size20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(Sizes.size32),
+            color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+            border: Border.all(
+              color: Colors.black.withValues(alpha: _isSelected ? 0 : 0.1),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.05),
+                blurRadius: 5,
+                spreadRadius: 5,
+              )
+            ]),
+        child: Text(
+          widget.interest,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: _isSelected ? Colors.white : Colors.black87,
           ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.05),
-              blurRadius: 5,
-              spreadRadius: 5,
-            )
-          ]),
-      child: Text(
-        interest,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
         ),
       ),
     );
