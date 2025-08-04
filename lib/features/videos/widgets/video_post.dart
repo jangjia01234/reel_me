843d648e7f7b37953954050c86e1fc5259c20bff
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-04 10:57:07 +0900

    animated play button 구현 (AnimationController addListener)

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -18,11 +18,16 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/sample-video.mp4");
 
-  bool isVideoPlaying = false;
+  final Duration _animationDuration = Duration(milliseconds: 200);
+
+  late final AnimationController _animationController;
+
+  bool _isPaused = false;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -36,7 +41,6 @@ class _VideoPostState extends State<VideoPost> {
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
     _videoPlayerController.play();
-    isVideoPlaying = true;
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -45,6 +49,17 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -62,12 +77,14 @@ class _VideoPostState extends State<VideoPost> {
   void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
-      isVideoPlaying = false;
+      _animationController.reverse();
     } else {
       _videoPlayerController.play();
-      isVideoPlaying = true;
+      _animationController.forward();
     }
-    setState(() {});
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
@@ -92,12 +109,17 @@ class _VideoPostState extends State<VideoPost> {
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: FaIcon(
-                  isVideoPlaying
-                      ? FontAwesomeIcons.pause
-                      : FontAwesomeIcons.play,
-                  color: Colors.white,
-                  size: Sizes.size52,
+                child: Transform.scale(
+                  scale: _animationController.value,
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animationDuration,
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
               ),
             ),
