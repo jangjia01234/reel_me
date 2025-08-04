49c9cabcffb1817496ac5aa591c6c99d57dc98af
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-04 15:03:25 +0900

    comment input 추가

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -11,12 +11,16 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  final String _userName = "니노";
+
   void _onClosePressed() {
     Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Container(
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
@@ -35,79 +39,105 @@ class _VideoCommentsState extends State<VideoComments> {
             )
           ],
         ),
-        body: ListView.separated(
-          padding: EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-            horizontal: Sizes.size16,
-          ),
-          separatorBuilder: (context, index) => Gaps.v20,
-          itemCount: 10,
-          itemBuilder: (context, index) => Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CircleAvatar(
-                radius: 18,
-                child: Text("니노"),
+        body: Stack(
+          children: [
+            ListView.separated(
+              padding: EdgeInsets.symmetric(
+                vertical: Sizes.size10,
+                horizontal: Sizes.size16,
               ),
-              Gaps.h10,
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '니꼬',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: Sizes.size14,
-                          color: Colors.grey.shade500),
-                    ),
-                    Gaps.v3,
-                    Text(
-                        "That's not it l've seen the same thing but also in a cave,"),
-                  ],
-                ),
-              ),
-              Gaps.h10,
-              Column(
+              separatorBuilder: (context, index) => Gaps.v20,
+              itemCount: 10,
+              itemBuilder: (context, index) => Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  FaIcon(
-                    FontAwesomeIcons.heart,
-                    size: Sizes.size20,
-                    color: Colors.grey.shade500,
+                  CircleAvatar(
+                    radius: 18,
+                    child: Text(_userName),
                   ),
-                  Gaps.v2,
-                  Text(
-                    '52.2K',
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
+                  Gaps.h10,
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          _userName,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500),
+                        ),
+                        Gaps.v3,
+                        Text(
+                            "That's not it l've seen the same thing but also in a cave,"),
+                      ],
                     ),
+                  ),
+                  Gaps.h10,
+                  Column(
+                    children: [
+                      FaIcon(
+                        FontAwesomeIcons.heart,
+                        size: Sizes.size20,
+                        color: Colors.grey.shade500,
+                      ),
+                      Gaps.v2,
+                      Text(
+                        '52.2K',
+                        style: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(
-            horizontal: Sizes.size10,
-            vertical: Sizes.size12,
-          ),
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: Sizes.size12,
-                backgroundColor: Colors.grey.shade500,
-                foregroundColor: Colors.white,
-                child: Center(
-                  child: Text(
-                    "니노",
-                    style: TextStyle(fontSize: Sizes.size10),
+            ),
+            Positioned(
+              bottom: 0,
+              width: size.width,
+              child: BottomAppBar(
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsetsGeometry.symmetric(
+                    horizontal: Sizes.size16,
+                    vertical: Sizes.size10,
+                  ),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.grey.shade500,
+                        foregroundColor: Colors.white,
+                        child: Text(_userName),
+                      ),
+                      Gaps.h10,
+                      Expanded(
+                        child: TextField(
+                          cursorColor: Theme.of(context).primaryColor,
+                          decoration: InputDecoration(
+                            hintText: "Add comment...",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                Sizes.size12,
+                              ),
+                              borderSide: BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: Sizes.size10,
+                              horizontal: Sizes.size12,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
