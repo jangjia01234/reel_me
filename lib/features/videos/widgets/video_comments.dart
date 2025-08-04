950bcf3b1660552620343aefa718d7aade5d389a
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-04 14:57:14 +0900

    comment layout 구성

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:reel_me/constants/gaps.dart';
 import 'package:reel_me/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -34,10 +35,78 @@ class _VideoCommentsState extends State<VideoComments> {
             )
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          padding: EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: Text("I'm a comment"),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text("니노"),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '니꼬',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500),
+                    ),
+                    Gaps.v3,
+                    Text(
+                        "That's not it l've seen the same thing but also in a cave,"),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.heart,
+                    size: Sizes.size20,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text(
+                    '52.2K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size10,
+            vertical: Sizes.size12,
+          ),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: Sizes.size12,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: Center(
+                  child: Text(
+                    "니노",
+                    style: TextStyle(fontSize: Sizes.size10),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
