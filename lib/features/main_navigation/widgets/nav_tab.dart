b5e980a8f3f467d9639102749f76e9cb90e61875
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-05 20:20:41 +0900

    tab bar 색상 전환 처리

--- lib/features/main_navigation/widgets/nav_tab.dart
+++ lib/features/main_navigation/widgets/nav_tab.dart
@@ -11,6 +11,7 @@ class NavTab extends StatelessWidget {
     required this.icon,
     required this.selectedIcon,
     required this.onTap,
+    required this.selectedIndex,
   });
 
   final String text;
@@ -18,6 +19,7 @@ class NavTab extends StatelessWidget {
   final IconData icon;
   final IconData selectedIcon;
   final Function onTap;
+  final int selectedIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -25,22 +27,22 @@ class NavTab extends StatelessWidget {
       child: GestureDetector(
         onTap: () => onTap(),
         child: Container(
-          color: Colors.black,
+          color: selectedIndex == 0 ? Colors.black : Colors.white,
           child: AnimatedOpacity(
             duration: Duration(milliseconds: 300),
-            opacity: isSelected ? 1 : 0.06,
+            opacity: isSelected ? 1 : 0.6,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 FaIcon(
                   isSelected ? selectedIcon : icon,
-                  color: Colors.white,
+                  color: selectedIndex == 0 ? Colors.white : Colors.black,
                 ),
                 Gaps.v5,
                 Text(
                   text,
                   style: TextStyle(
-                    color: Colors.white,
+                    color: selectedIndex == 0 ? Colors.white : Colors.black,
                   ),
                 )
               ],
