25ca7b379d560441d72a975c14d1c7dc7917c28d
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-12 00:41:15 +0900

    SwitchListTile, CheckboxListTile 구현

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +25,20 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          // 방법 1
+          SwitchListTile(
+            activeColor: Theme.of(context).primaryColor,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("Subtitle"),
+          ),
+          CheckboxListTile(
+            activeColor: Theme.of(context).primaryColor,
+            value: _notifications,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+            subtitle: const Text("Subtitle"),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
@@ -48,7 +75,6 @@ class SettingsScreen extends StatelessWidget {
             },
             title: const Text("What is your birthday?"),
           ),
-          // 방법 2
           const AboutListTile(),
         ],
       ),
